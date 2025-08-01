5e488c336e01ffd0731003cb4e62bf01bd33df62
kadmiebihamleontine <leontinekadmie@gmail.com>
2025-08-01 22:01:19 +0000

    jour 10

---
+++ Day_8/Dictionnaire.py
@@ -0,0 +1,13 @@
+#Exercices: Jour 8
+#1/
+dog = {}
+#2/
+dog = {'name':'Jojo', 'color':'maron', 'breed':'Bouledogue', 'legs':'court', 'age':5}
+#3/
+student = {'first_name':'Toto', 'last_name':'ABALO', 'gender':'Masculin', 'age':'15',
+'marital_status':'single', 'skills':{'informatique','Maths','Physique','Anglais','ECM'}, 'country':'Togo', 'city':'Lomé', 'address':'AGOE'}
+#4/
+print(len(student))
+#5/
+skills_values = student['skills'].value()
+print(skills_values)
